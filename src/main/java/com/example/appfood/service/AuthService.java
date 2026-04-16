9653189b6263e3dec44e9ca84396592618a3cf08
BealyKing <Zhivnitski@mail.ru>
2026-04-16 16:56:49 +1000

    save

--- src/main/java/com/example/appfood/service/AuthService.java
+++ src/main/java/com/example/appfood/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.appfood.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
